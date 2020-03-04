7f77bb3e10a4c9aadd3b9fa5ffbdec5511d28625
mholwill <m_holwill@hotmail.co.uk>
2020-03-04 11:05:51 +0000

    CRUD functions complete for server side

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -25,6 +25,49 @@ const createRouter = function(collection) {
      });
    });
 
+   router.post('/', (req, res) => {
+     const newData = req.body;
+     collection.insertOne(newData)
+     .then((result) => {
+       res.json(result.ops[0])
+     })
+     .catch((err) => {
+       console.error(err)
+       res.status(500);
+       res.json({status: 500, error: err});
+     });
+   });
+
+   router.put('/:id', (req, res) => {
+     const id = req.params.id;
+     const updateData = req.body;
+     collection.findOneAndUpdate(
+       {_id: ObjectId(id)},
+       {$set: updateData},
+       {returnOriginal: false}
+     )
+     .then((result) => {
+       res.json(result.value)
+     })
+     .catch((err) => {
+       console.error(err)
+       res.status(500);
+       res.json({status: 500, error: err});
+     });
+   });
+
+   router.delete('/:id', (req, res) => {
+     const id = req.params.id;
+     collection.deleteOne({_id: ObjectId(id)})
+     .then((result) => {res.json(result)
+   })
+     .catch((err) => {
+       console.error(err)
+       res.status(500);
+       res.json({status: 500, error: err});
+     });
+   });
+
   return router;
 }
 
