74ceea2d25416a8696158ee22cccea25dd04d76e
mholwill <m_holwill@hotmail.co.uk>
2020-03-04 10:49:59 +0000

    Show and show all RESTful routes added

---
+++ server/db/seeds.js
@@ -0,0 +1,23 @@
+use guests;
+db.dropDatabase();
+
+db.bookings.insertMany([
+  {
+    name: "Graham Grahamsson",
+    email: "g_g@gmail.com",
+    numberOfGuests: 1,
+    checkedIn: false
+  },
+  {
+    name: "Callum Gould",
+    email: "callum@gould.co.uk",
+    numberOfGuests: 6,
+    checkedIn: true
+  },
+  {
+    name: "Eugene Traitor",
+    email: "traitor@codeclan.com",
+    numberOfGuests: 2,
+    checkedIn: false
+  }
+]);
